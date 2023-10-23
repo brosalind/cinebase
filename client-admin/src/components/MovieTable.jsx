a54ab524d1060192ca859767580fb35730ed2caf
brosalind <benita.jenniefer@gmail.com>
2023-10-23 16:28:12 +0700

    feat: add search function

--- client-admin/src/components/MovieTable.jsx
+++ client-admin/src/components/MovieTable.jsx
@@ -1,9 +1,9 @@
 import useFetch from "../hooks/useFetch";
-import { Table } from 'react-bootstrap'
+import { Table, Form, Button } from 'react-bootstrap'
 import MovieTableRow from '../components/MovieTableRow'
 import { useSelector, useDispatch } from 'react-redux'
 import { thunkFetchMovies } from "../stores/movieActionCreator";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import '../assets/movie.css';
@@ -12,56 +12,91 @@ import '../assets/movie.css';
 
 
 function MovieTable() {
-    const {movies, loading} = useSelector((state) => {
+    const { movies, loading } = useSelector((state) => {
         return state.movie
     })
+    const [search, setSearch] = useState('')
+
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(thunkFetchMovies())
     }, [])
 
-    if(loading) {
-        return<>
-        <Container className="pt-5">
-           <Col>
-       <h1 className="text-center">Loading... Please wait.</h1>
-       </Col>
-       </Container>
-       </>
+    function handleSearchChange(e) {
+        setSearch(e.target.value);
+    };
+
+    const [filteredMovies, setFilteredMovies] = useState([])
+
+    useEffect(() => {
+        const filtered = movies.filter((movie) =>{
+            const searchTerm = search.toLowerCase();
+            return (
+                movie.title.toLowerCase().includes(searchTerm) ||
+                movie.writer.toLowerCase().includes(searchTerm) ||
+                movie.director.toLowerCase().includes(searchTerm)  ||
+                movie.Casts.some((cast) => cast.name.toLowerCase().includes(searchTerm))
+            );
+        });
+        setFilteredMovies(filtered);
+    }, [search, movies]);
+
+
+
+    if (loading) {
+        return <>
+            <Container className="pt-5">
+                <Col>
+                    <h1 className="text-center">Loading... Please wait.</h1>
+                </Col>
+            </Container>
+        </>
     }
 
 
-return (
-    <>
-    <Container>
-    <div className="table-responsive custom-table-container">
-    <Table className="table-responsive custom-table">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Title</th>
-                <th>Synopsis</th>
-                <th>Trailer</th>
-                <th>Writer</th>
-                <th>Director</th>
-                <th>Rating</th>
-                <th>Genre</th>
-                <th>Owner</th>
-                <th>Casts</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <MovieTableRow movies={movies}></MovieTableRow>
-        </tbody>
-        
-    </Table>
-    </div>
-    </Container>
-    </>
+    return (
+        <>
 
-)
+            <Form className="d-flex mb-5">
+                <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                    value={search}
+                    onChange={handleSearchChange}
+                />
+                <Button variant="outline-success">Search</Button>
+            </Form>
+            <Container>
+                <div className="table-responsive custom-table-container">
+                    <Table className="table-responsive custom-table">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Title</th>
+                                <th>Synopsis</th>
+                                <th>Trailer</th>
+                                <th>Writer</th>
+                                <th>Director</th>
+                                <th>Rating</th>
+                                <th>Genre</th>
+                                <th>Owner</th>
+                                <th>Casts</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <MovieTableRow movies={filteredMovies}></MovieTableRow>
+                        </tbody>
+
+                    </Table>
+                </div>
+            </Container>
+        </>
+
+    )
 }
 
 
--- client-admin/src/pages/Movie.jsx
+++ client-admin/src/pages/Movie.jsx
@@ -12,7 +12,7 @@ import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col"
 function Movie() {
     const dispatch = useDispatch()
-    const {genres, fetchGenresLoading, fetchGenresError} = useSelector((state) => {
+    const { genres, fetchGenresLoading, fetchGenresError } = useSelector((state) => {
         return state.genre
     })
     const { show, handleClose, handleShow } = useToggle(false)
@@ -29,13 +29,14 @@ function Movie() {
         authorId: 1,
         casts: []
     })
-    
+
+
     useEffect(() => {
         dispatch(thunkFetchGenres())
         dispatch(thunkFetchMovies())
     }, [])
-      
-   
+
+
 
     function handleChange(e) {
         const { value, name } = e.target
@@ -46,7 +47,7 @@ function Movie() {
     }
 
     function handleCastChange(e, index) {
-        const {name, value} = e.target
+        const { name, value } = e.target
         const cast = [...movieForm.casts];
         cast[index][name] = value
         setMovieForm({
@@ -78,119 +79,121 @@ function Movie() {
         })
     }
 
-    if(fetchGenresLoading || !genres.length){
+    if (fetchGenresLoading || !genres.length) {
         return <>
-         <Container className="pt-5">
-            <Col>
-        <h1 className="text-center">Loading... Please wait.</h1>
-        </Col>
-        </Container>
+            <Container className="pt-5">
+                <Col>
+                    <h1 className="text-center">Loading... Please wait.</h1>
+                </Col>
+            </Container>
         </>
     }
-    
+
+
+
 
     return (
         <>
-       <Container className='d-flex justify-content-center' style={{ margin: '0 auto', marginBottom: '30px', marginTop: '30px'  }}>
-        <div>
+            <Container className='d-flex justify-content-center' style={{ margin: '0 auto', marginBottom: '30px', marginTop: '30px' }}>
+                <div>
                     <h6
                         className="sidebar-heading px-3 mt-4 mb-1 text-muted text-uppercase" style={{ textAlign: 'center' }}>
                         currently viewing</h6>
-                    <h3 style={{ textAlign: 'center', marginBottom: '30px' }}>Movie Database</h3>    
+                    <h3 style={{ textAlign: 'center', marginBottom: '30px' }}>Movie Database</h3>
                     <div className="d-flex justify-content-center mb-5">
-            <Button variant="outline-dark" onClick={handleShow} className="custom-button" style={{marginLeft: '10px'}}>
-                Add a New Movie
-            </Button>
-            </div>
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Add a New Movie</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Movie Title</Form.Label>
-                            <Form.Control type="text" placeholder="Enter a new movie name..." name="title" value={movieForm.title} onChange={handleChange} />
+                        <Button variant="outline-dark" onClick={handleShow} className="custom-button" style={{ marginLeft: '10px' }}>
+                            Add a New Movie
+                        </Button>
+                    </div>
 
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Movie Synopsis</Form.Label>
-                            <Form.Control type="text" placeholder="Enter a new movie synopsis..." name="synopsis" value={movieForm.synopsis} onChange={handleChange} />
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Movie Poster</Form.Label>
-                            <Form.Control type="text" placeholder="Enter a new movie poster..." name="imgUrl" value={movieForm.imgUrl} onChange={handleChange} />
-                        </Form.Group>
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Add a New Movie</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Movie Title</Form.Label>
+                                    <Form.Control type="text" placeholder="Enter a new movie name..." name="title" value={movieForm.title} onChange={handleChange} />
 
-                        <Form.Group className="mb-3">
-                            <Form.Label>Movie Trailer</Form.Label>
-                            <Form.Control type="text" placeholder="Enter a new movie trailer..." name="trailerUrl" value={movieForm.trailerUrl} onChange={handleChange} />
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Writer</Form.Label>
-                            <Form.Control type="text" placeholder="Enter a new movie writer..." name="writer" value={movieForm.writer} onChange={handleChange} />
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Director</Form.Label>
-                            <Form.Control type="text" placeholder="Enter a new movie director..." name="director" value={movieForm.director} onChange={handleChange} />
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Year</Form.Label>
-                            <Form.Control type="text" placeholder="Enter a new movie year..." name="year" value={movieForm.year} onChange={handleChange} />
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Movie Rating</Form.Label>
-                            <Form.Control type="number" placeholder="Enter a new movie rating..." name="rating" value={movieForm.rating} onChange={handleChange} />
-                        </Form.Group>
+                                </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Movie Synopsis</Form.Label>
+                                    <Form.Control type="text" placeholder="Enter a new movie synopsis..." name="synopsis" value={movieForm.synopsis} onChange={handleChange} />
+                                </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Movie Poster</Form.Label>
+                                    <Form.Control type="text" placeholder="Enter a new movie poster..." name="imgUrl" value={movieForm.imgUrl} onChange={handleChange} />
+                                </Form.Group>
 
-                        <Form.Label>Select a Movie Genre</Form.Label>
-                        <Form.Select aria-label="Default select example" value={movieForm.genre} onChange={handleChange} name="genreId" className="mb-3">
-                            <option disabled value="">Open this select menu</option>
-                            {
-                                genres.map((el, index)=> {
-                                    return <option value={el.id} key={index}>{el.name}</option>
-                                })
-                            }
-                        </Form.Select>
-                        {
-                            movieForm.casts.map((el, index) => {
-                                return (
-                                    <>
-                                    <div key={index + 1}>
-                                    <Form.Group key={index} className="mb-3">
-                                        
-                                        <Form.Label>Cast Name</Form.Label>
-                                        <Form.Control type="text" placeholder="Enter a new cast name..." name="name" value={movieForm.casts[index].name} onChange={(e) => {handleCastChange(e, index)}} />
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Movie Trailer</Form.Label>
+                                    <Form.Control type="text" placeholder="Enter a new movie trailer..." name="trailerUrl" value={movieForm.trailerUrl} onChange={handleChange} />
+                                </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Writer</Form.Label>
+                                    <Form.Control type="text" placeholder="Enter a new movie writer..." name="writer" value={movieForm.writer} onChange={handleChange} />
+                                </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Director</Form.Label>
+                                    <Form.Control type="text" placeholder="Enter a new movie director..." name="director" value={movieForm.director} onChange={handleChange} />
+                                </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Year</Form.Label>
+                                    <Form.Control type="text" placeholder="Enter a new movie year..." name="year" value={movieForm.year} onChange={handleChange} />
+                                </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Movie Rating</Form.Label>
+                                    <Form.Control type="number" placeholder="Enter a new movie rating..." name="rating" value={movieForm.rating} onChange={handleChange} />
+                                </Form.Group>
 
-                                    </Form.Group>
-                                        <Form.Group className="mb-3" key={index}>
-                                            <Form.Label>Cast Profile Pict</Form.Label>
-                                            <Form.Control type="text" placeholder="Enter a new cast profile pict..." name="profilePict" value={movieForm.casts[index].profilePict} onChange={(e) => {handleCastChange(e, index)}}/>
-                                        </Form.Group>
-                                        </div>
-                                    </>
-                                )
-                            }
-                            )}
+                                <Form.Label>Select a Movie Genre</Form.Label>
+                                <Form.Select aria-label="Default select example" value={movieForm.genre} onChange={handleChange} name="genreId" className="mb-3">
+                                    <option disabled value="">Open this select menu</option>
+                                    {
+                                        genres.map((el, index) => {
+                                            return <option value={el.id} key={index}>{el.name}</option>
+                                        })
+                                    }
+                                </Form.Select>
+                                {
+                                    movieForm.casts.map((el, index) => {
+                                        return (
+                                            <>
+                                                <div key={index + 1}>
+                                                    <Form.Group key={index} className="mb-3">
 
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="outline-dark" onClick={addNewInput}>
-                        Add Cast Member
-                    </Button>
-                    <Button variant="dark" onClick={handleSubmit}>
-                        Submit
-                    </Button>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+                                                        <Form.Label>Cast Name</Form.Label>
+                                                        <Form.Control type="text" placeholder="Enter a new cast name..." name="name" value={movieForm.casts[index].name} onChange={(e) => { handleCastChange(e, index) }} />
 
+                                                    </Form.Group>
+                                                    <Form.Group className="mb-3" key={index}>
+                                                        <Form.Label>Cast Profile Pict</Form.Label>
+                                                        <Form.Control type="text" placeholder="Enter a new cast profile pict..." name="profilePict" value={movieForm.casts[index].profilePict} onChange={(e) => { handleCastChange(e, index) }} />
+                                                    </Form.Group>
+                                                </div>
+                                            </>
+                                        )
+                                    }
+                                    )}
 
-            <MovieTable></MovieTable>
-            </div>
+                            </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="outline-dark" onClick={addNewInput}>
+                                Add Cast Member
+                            </Button>
+                            <Button variant="dark" onClick={handleSubmit}>
+                                Submit
+                            </Button>
+                            <Button variant="secondary" onClick={handleClose}>
+                                Close
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+
+                    <MovieTable></MovieTable>
+                </div>
             </Container>
         </>
     )
